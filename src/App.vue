9e39a28a9e9b3c3a98fc99393eda5809efe0daff
Fumeng233 <1173212531@qq.com>
2021-10-18 00:00:55 +0800

    color

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #50332c;
   margin-top: 60px;
 }
 </style>
